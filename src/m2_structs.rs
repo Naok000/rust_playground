4a96152b0cb5a449b0fdd68b6b4543e4281b1ce6
Nao k <light.kart92@gmail.com>
2023-09-30 22:50:16 +0900

    learning Structs

---
+++ src/m2_structs.rs
@@ -0,0 +1,54 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool
+}
+
+// 値の読み取りのみであれば&mutを宣言する必要はない
+impl User {
+    fn increment_signin_count(&mut self){
+        self.sign_in_count += 1
+    }
+
+    fn change_email(&mut self, new_email: &str){
+        self.email = String::from(new_email)
+    }
+
+    fn isActive(&mut self){
+        self.active = !self.active
+    }
+}
+
+// どのようなユーザーであっても変更可能な参照を渡すことができる
+// &mut - Pointer types, Stack
+fn change_username(user: &mut User, new_username: &str){
+    user.username = String::from(new_username)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn tests_structs() {
+        let mut user_1: User = User {
+            username: String::from("someusername1"),
+            email: String::from("someone@example.com"),
+            active: true,
+            sign_in_count: 1
+        };
+
+        let mut user_2: User = User {
+            username: String::from("someusername2"),
+            email: String::from("someon2@example.com"),
+            active: false,
+            sign_in_count: 0
+        };
+
+        change_username(&mut user_1,"somenewusername");
+
+        dbg!(user_1);
+    }
+}
