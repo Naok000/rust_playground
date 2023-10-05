54c66cff087ac81447b5a41984acbc69bbbd6476
Nao k <light.kart92@gmail.com>
2023-10-05 23:09:03 +0900

    learned patterns match

---
+++ src/m6_patterns.rs
@@ -0,0 +1,25 @@
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn tests_match_literals() {
+        let number: i32 = 20;
+
+        let res: &str = match number {
+            1 => "It was the first!",
+            2 | 3 | 5 | 7 | 15 | 20 => "We found it in the sequence!",
+            _ => "It was something else!",
+        };
+
+        println!("{}",res);
+    }
+
+    #[test]
+    fn test_match_option() {
+        
+    }
+}
