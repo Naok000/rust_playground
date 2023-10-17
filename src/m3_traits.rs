cacd3c579bb67c6fd4f7f843ddd0c6d91c9e75f7
Nao k <light.kart92@gmail.com>
2023-10-17 16:00:50 +0900

    code cleanup

--- src/m3_traits.rs
+++ src/m3_traits.rs
@@ -1,43 +1,40 @@
-
-trait Attacker {
-    fn choose_style(&self) -> String;
-    fn choose_weapon(&self) -> String;
-}
-
-#[derive(Debug)]
-enum Character {
-    Warrior,
-    Archer,
-    Wizard
-}
-
-impl Attacker for Character {
-    fn choose_style(&self) -> String {
-        match self {
-            Character::Warrior => "wing chun".to_string(),
-            Character::Archer => "kung fu".to_string(),
-            Character::Wizard => "thai chi".to_string(),
-        }
-    }
-    fn choose_weapon(&self) -> String {
-        match self {
-            Character::Warrior => "fist".to_string(),
-            Character::Archer => "bow".to_string(),
-            Character::Wizard => "Magic Stick".to_string(),
-        }
-    }
-}
-
 #[cfg(test)]
 mod test {
-    use super::*;
+    trait Attacker {
+        fn choose_style(&self) -> String;
+        fn choose_weapon(&self) -> String;
+    }
+
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    enum Character {
+        Warrior,
+        Archer,
+        Wizard,
+    }
+
+    impl Attacker for Character {
+        fn choose_style(&self) -> String {
+            match self {
+                Character::Warrior => "wing chun".to_string(),
+                Character::Archer => "kung fu".to_string(),
+                Character::Wizard => "thai chi".to_string(),
+            }
+        }
+        fn choose_weapon(&self) -> String {
+            match self {
+                Character::Warrior => "fist".to_string(),
+                Character::Archer => "bow".to_string(),
+                Character::Wizard => "Magic Stick".to_string(),
+            }
+        }
+    }
 
     #[test]
     fn tests_traits() {
         let my_character: Character = Character::Warrior;
         let chosen_fighting_style = my_character.choose_style();
         let chosen_fighting_weapon = my_character.choose_weapon();
-        
 
         dbg!(chosen_fighting_style);
         dbg!(chosen_fighting_weapon);
--- src/m5_lifetimes.rs
+++ src/m5_lifetimes.rs
@@ -1,58 +1,53 @@
-#[allow(dead_code,unused_variables)]
+#[allow(dead_code, unused_variables)]
 fn example0() {
-    
-    let r; 
+    let r;
 
-    let x = 5;        
-    r = &x;               
-                              
-     println!("r: {}", r); 
+    let x = 5;
+    r = &x;
+
+    println!("r: {}", r);
 }
 
-#[allow(dead_code,unused_variables)]
-fn example1 () {
+#[allow(dead_code, unused_variables)]
+fn example1() {
     // Allocate space in memory
     let highest_age: i32;
 
     // Initialize vars
-    let alice_age: i32 = 20;    // 'a
-    let bob_age: i32 = 21;      // 'b : 'a 有効期限がaと一緒
+    let alice_age: i32 = 20; // 'a
+    let bob_age: i32 = 21; // 'b : 'a 有効期限がaと一緒
 
     // Call function
     highest_age = lagest(&alice_age, &bob_age);
 
-    println!("Highest age is {}",highest_age);
+    println!("Highest age is {}", highest_age);
 
     fn lagest(compare_1: &i32, compare_2: &i32) -> i32 {
         if compare_1 > compare_2 {
-            *compare_1  // 逆参照
+            *compare_1 // 逆参照
         } else {
             *compare_2
         }
     }
 }
 
-
-#[allow(dead_code,unused_variables)]
-fn example2 () {
+#[allow(dead_code, unused_variables)]
+fn example2() {
     // Allocate space in memory
     let highest_age: &i32;
     let new_value: i32;
 
     // Initialize vars
-    let alice_age: i32 = 20;    // 'a
-    
-    
+    let alice_age: i32 = 20; // 'a
 
     {
-        let bob_age: i32 = 21;      // 'b : 'a 有効期限がaと一緒 
-        // Call function
+        let bob_age: i32 = 21; // 'b : 'a 有効期限がaと一緒
+                               // Call function
         highest_age = lagest(&alice_age, &bob_age);
         new_value = *highest_age;
     } // 'b out of scope
-   
 
-    println!("New value is {}",new_value);
+    println!("New value is {}", new_value);
 
     fn lagest<'a>(compare_1: &'a i32, compare_2: &'a i32) -> &'a i32 {
         if compare_1 > compare_2 {
@@ -63,26 +58,23 @@ fn example2 () {
     }
 }
 
-#[allow(dead_code,unused_variables)]
-fn example_3_generics () {
+#[allow(dead_code, unused_variables)]
+fn example_3_generics() {
     // Allocate space in memory
     let highest_age: &i32;
     let new_value: i32;
 
     // Initialize vars
-    let alice_age: i32 = 20;    // 'a
-    
-    
+    let alice_age: i32 = 20; // 'a
 
     {
-        let bob_age: i32 = 21;      // 'b : 'a 有効期限がaと一緒 
-        // Call function
+        let bob_age: i32 = 21; // 'b : 'a 有効期限がaと一緒
+                               // Call function
         highest_age = lagest::<i32>(&alice_age, &bob_age);
         new_value = *highest_age;
     } // 'b out of scope
-   
 
-    println!("New value is {}",new_value);
+    println!("New value is {}", new_value);
 
     fn lagest<'a, T: PartialOrd>(compare_1: &'a T, compare_2: &'a T) -> &'a T {
         if compare_1 > compare_2 {
@@ -93,33 +85,35 @@ fn example_3_generics () {
     }
 }
 
-#[allow(dead_code,unused_variables)]
-struct Person<'p,'q> {
+#[allow(dead_code, unused_variables)]
+struct Person<'p, 'q> {
     name: &'p str,
-    points: &'q f32
+    points: &'q f32,
 }
 
-
-#[allow(dead_code,unused_variables)]
-fn example_4_struct () {
+#[allow(dead_code, unused_variables)]
+fn example_4_struct() {
     // Allocate space in memory
     let highest_points: &f32;
     let new_value: f32;
 
     // Initialize vars
-    let alice: Person = Person { name: "alice", points: &50.2 };
-    
-    
+    let alice: Person = Person {
+        name: "alice",
+        points: &50.2,
+    };
 
     {
-        let bob: Person = Person { name: "bob", points: &40.5 };
+        let bob: Person = Person {
+            name: "bob",
+            points: &40.5,
+        };
         // Call function
         highest_points = lagest::<f32>(&alice.points, &bob.points);
         new_value = *highest_points;
     } // 'b out of scope
-   
 
-    println!("New value is {}",new_value);
+    println!("New value is {}", new_value);
 
     fn lagest<'a, T: PartialOrd>(compare_1: &'a T, compare_2: &'a T) -> &'a T {
         if compare_1 > compare_2 {
@@ -129,14 +123,11 @@ fn example_4_struct () {
         }
     }
 }
-
-
 
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn tests_lifetimes() {
-    }
+    fn tests_lifetimes() {}
 }
